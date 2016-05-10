af4ff1606bdd15d890cbe5b1705886e5c713fd2f
Herbert Valerio Riedel <hvr@gnu.org>
2016-05-10 13:36:49 +0200

    Rewrite test-suite

---
+++ src-tests/test-sha256.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Lazy   as BL
+import qualified Data.ByteString.Base16 as B16
+
+-- reference implementation
+import qualified Data.Digest.Pure.SHA as REF
+
+-- implementation under test
+import qualified Crypto.Hash.SHA256     as IUT
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck as QC
+
+vectors :: [ByteString]
+vectors =
+    [ ""
+    , "The quick brown fox jumps over the lazy dog"
+    , "The quick brown fox jumps over the lazy cog"
+    , "abc"
+    , "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+    , "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"
+    , B.replicate 1000000 0x61
+    ]
+
+answers :: [ByteString]
+answers = map (B.filter (/= 0x20))
+    [ "e3b0c442 98fc1c14 9afbf4c8 996fb924 27ae41e4 649b934c a495991b 7852b855"
+    , "d7a8fbb3 07d78094 69ca9abc b0082e4f 8d5651e4 6d3cdb76 2d02d0bf 37c9e592"
+    , "e4c4d8f3 bf76b692 de791a17 3e053211 50f7a345 b46484fe 427f6acc 7ecc81be"
+    , "ba7816bf 8f01cfea 414140de 5dae2223 b00361a3 96177a9c b410ff61 f20015ad"
+    , "248d6a61 d20638b8 e5c02693 0c3e6039 a33ce459 64ff2167 f6ecedd4 19db06c1"
+    , "cf5b16a7 78af8380 036ce59e 7b049237 0b249b11 e8f07a51 afac4503 7afee9d1"
+    , "cdc76e5c 9914fb92 81a1c7e2 84d73e67 f1809a48 a497200e 046d39cc c7112cd0"
+    ]
+
+ansXLTest :: ByteString
+ansXLTest = B.filter (/= 0x20)
+    "50e72a0e 26442fe2 552dc393 8ac58658 228c0cbf b1d2ca87 2ae43526 6fcd055e"
+
+katTests :: [TestTree]
+katTests
+  | length vectors == length answers = map makeTest (zip3 [1::Int ..] vectors answers) ++ [xltest]
+  | otherwise = error "vectors/answers length mismatch"
+  where
+    makeTest (i, v, r) = testGroup ("vec"++show i) $
+        [ testCase "one-pass" (r @=? runTest v)
+        , testCase "inc-1"    (r @=? runTestInc 1 v)
+        , testCase "inc-2"    (r @=? runTestInc 2 v)
+        , testCase "inc-3"    (r @=? runTestInc 3 v)
+        , testCase "inc-4"    (r @=? runTestInc 4 v)
+        , testCase "inc-5"    (r @=? runTestInc 5 v)
+        , testCase "inc-7"    (r @=? runTestInc 7 v)
+        , testCase "inc-8"    (r @=? runTestInc 8 v)
+        , testCase "inc-9"    (r @=? runTestInc 9 v)
+        , testCase "inc-16"   (r @=? runTestInc 16 v)
+        , testCase "lazy-1"   (r @=? runTestLazy 1 v)
+        , testCase "lazy-2"   (r @=? runTestLazy 2 v)
+        , testCase "lazy-7"   (r @=? runTestLazy 7 v)
+        , testCase "lazy-8"   (r @=? runTestLazy 8 v)
+        , testCase "lazy-16"  (r @=? runTestLazy 16 v)
+        ]
+
+    runTest :: ByteString -> ByteString
+    runTest = B16.encode . IUT.hash
+
+    runTestInc :: Int -> ByteString -> ByteString
+    runTestInc i = B16.encode . IUT.finalize . myfoldl' IUT.update IUT.init . splitB i
+
+    runTestLazy :: Int -> ByteString -> ByteString
+    runTestLazy i = B16.encode . IUT.hashlazy . BL.fromChunks . splitB i
+
+    ----
+
+    xltest = testGroup "XL-vec"
+        [ testCase "inc" (ansXLTest @=? (B16.encode . IUT.hashlazy) vecXL) ]
+      where
+        vecXL = BL.fromChunks (replicate 16777216 "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno")
+
+    splitB :: Int -> ByteString -> [ByteString]
+    splitB l b
+      | B.length b > l = b1 : splitB l b2
+      | otherwise = [b]
+      where
+        (b1, b2) = B.splitAt l b
+
+
+-- define own 'foldl' here to avoid RULE rewriting to 'hashlazy'
+myfoldl' :: (b -> a -> b) -> b -> [a] -> b
+myfoldl' f z0 xs0 = lgo z0 xs0
+  where
+    lgo z []     = z
+    lgo z (x:xs) = let z' = f z x
+                   in z' `seq` lgo z' xs
+
+newtype RandBS = RandBS { unRandBS :: ByteString }
+newtype RandLBS = RandLBS BL.ByteString
+
+instance Arbitrary RandBS where
+    arbitrary = fmap (RandBS . B.pack) arbitrary
+    shrink (RandBS x) = fmap RandBS (go x)
+      where
+        go bs = zipWith B.append (B.inits bs) (tail $ B.tails bs)
+
+instance Show RandBS where
+    show (RandBS x) = "RandBS {len=" ++ show (B.length x)++"}"
+
+instance Arbitrary RandLBS where
+    arbitrary = fmap (RandLBS . BL.fromChunks . map unRandBS) arbitrary
+
+instance Show RandLBS where
+    show (RandLBS x) = "RandLBS {len=" ++ show (BL.length x) ++ ", chunks=" ++ show (length $ BL.toChunks x)++"}"
+
+
+refImplTests :: [TestTree]
+refImplTests =
+    [ testProperty "hash" prop_hash
+    , testProperty "hashlazy" prop_hashlazy
+    ]
+  where
+    prop_hash (RandBS bs)
+        = ref_hash bs == IUT.hash bs
+
+    prop_hashlazy (RandLBS bs)
+        = ref_hashlazy bs == IUT.hashlazy bs
+
+    ref_hash :: ByteString -> ByteString
+    ref_hash = BL.toStrict . REF.bytestringDigest . REF.sha256 . BL.fromStrict
+
+    ref_hashlazy :: BL.ByteString -> ByteString
+    ref_hashlazy = BL.toStrict . REF.bytestringDigest . REF.sha256
+
+
+main :: IO ()
+main = defaultMain $ testGroup "cryptohash-sha256"
+    [ testGroup "KATs" katTests
+    , testGroup "REF" refImplTests
+    ]
